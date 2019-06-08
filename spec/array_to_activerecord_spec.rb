9ef58202a10cace84b723c2329eafb91c4d847e7
potato2003 <potato2003@gmail.com>
2019-06-08 20:56:07 +0900

    improva test coverage

--- spec/array_to_activerecord_spec.rb
+++ spec/array_to_activerecord_spec.rb
@@ -10,11 +10,22 @@ RSpec.describe ArrayToActiverecord do
       let(:customer_a) { create_customer(name: "customer_a") }
       let(:customer_b) { create_customer(name: "customer_b") }
       let(:customer_c) { create_customer(name: "customer_c") }
+      let(:not_included_customer) { create_customer(name: "not_included_customer") }
       let(:array) { [ customer_a, customer_b, customer_c ] }
 
       it { is_expected.to be_kind_of(ActiveRecord::Relation) }
-      it("should match ids") { expect(subject.ids).to match(array.map(&:id)) }
+      it("should match ids") { expect(subject.ids).to match_array(array.map(&:id)) }
       it("has where clause") { expect(subject.to_sql).to include("WHERE", "IN (#{subject.ids.join(", ")}") }
+
+      it("returns Relation that can call to the find correctly") do
+        expect(subject.find(customer_a.id)).to eq(customer_a)
+        expect{ subject.find(not_included_customer.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it("returns Relation that can call to the where correctly") do
+        expect(subject.where(id: [customer_a.id, customer_b.id, not_included_customer.id])).to contain_exactly(customer_a, customer_b)
+        expect(subject.where(id: [not_included_customer.id])).to be_empty
+      end
     end
 
     context "when give an array including non-ActiveRecord instances" do
